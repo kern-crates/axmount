505d17edad334ae34e0821cdc26c0c52a2043841
Shi Lei <shi_lei@massclouds.com>
2024-05-20 07:39:35 +0000

    support virtio_blk

--- rt_axmount/src/lib.rs
+++ rt_axmount/src/lib.rs
@@ -9,6 +9,7 @@ use core::panic::PanicInfo;
 use axtype::{align_up_4k, align_down_4k, phys_to_virt, virt_to_phys};
 use driver_block::{ramdisk, BlockDriverOps};
 use axdriver::{prelude::*, AxDeviceContainer};
+use axhal::mem::memory_regions;
 use fstree::FsStruct;
 
 /// Entry
@@ -26,6 +27,9 @@ pub extern "Rust" fn runtime_main(cpu_id: usize, _dtb_pa: usize) {
     let end = align_down_4k(axconfig::PHYS_MEMORY_END);
     axalloc::global_init(phys_to_virt(start), end - start);
 
+    info!("Initialize kernel page table...");
+    remap_kernel_memory().expect("remap kernel memoy failed");
+
     let ctx = Arc::new(taskctx::init_sched_info());
     unsafe {
         let ptr = Arc::into_raw(ctx.clone());
@@ -36,10 +40,12 @@ pub extern "Rust" fn runtime_main(cpu_id: usize, _dtb_pa: usize) {
     run_queue::init();
 
     {
-        let mut disk = ramdisk::RamDisk::new(0x10000);
+        //let mut disk = ramdisk::RamDisk::new(0x10000);
+        let mut alldevs = axdriver::init_drivers();
+        let mut disk = alldevs.block.take_one().unwrap();
         let mut disk = AxDeviceContainer::from_one(disk);
 
-        let main_fs = axmount::init_filesystems(disk, true);
+        let main_fs = axmount::init_filesystems(disk, false);
         let root_dir = axmount::init_rootfs(main_fs);
         let mut fs = FsStruct::new();
         fs.init(root_dir);
@@ -57,6 +63,26 @@ pub extern "Rust" fn runtime_main(cpu_id: usize, _dtb_pa: usize) {
 
     info!("[rt_axmount]: ok!");
     axhal::misc::terminate();
+}
+
+fn remap_kernel_memory() -> Result<(), axhal::paging::PagingError> {
+    use axhal::paging::PageTable;
+    use axhal::paging::{reuse_page_table_root, setup_page_table_root};
+    use axhal::mem::phys_to_virt;
+
+    let mut kernel_page_table = PageTable::try_new()?;
+    for r in memory_regions() {
+        kernel_page_table.map_region(
+            phys_to_virt(r.paddr),
+            r.paddr,
+            r.size,
+            r.flags.into(),
+            true,
+        )?;
+    }
+    setup_page_table_root(kernel_page_table);
+
+    Ok(())
 }
 
 pub fn panic(info: &PanicInfo) -> ! {
