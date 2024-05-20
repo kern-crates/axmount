5797b67484139eb846f23cd8e9a6785aefc8dc15
Shi Lei <shi_lei@massclouds.com>
2024-05-20 02:14:28 +0000

    add tests for dir and files

--- rt_axmount/src/lib.rs
+++ rt_axmount/src/lib.rs
@@ -9,15 +9,18 @@ use core::panic::PanicInfo;
 use axtype::{align_up_4k, align_down_4k, phys_to_virt, virt_to_phys};
 use driver_block::{ramdisk, BlockDriverOps};
 use axdriver::{prelude::*, AxDeviceContainer};
+use fstree::FsStruct;
 
 /// Entry
 #[no_mangle]
 pub extern "Rust" fn runtime_main(cpu_id: usize, _dtb_pa: usize) {
-    axhal::cpu::init_primary(cpu_id);
+    assert_eq!(cpu_id, 0);
 
     axlog2::init();
     axlog2::set_max_level("debug");
-    info!("[rt_axmount]: ...");
+    info!("[rt_axmount]: ... cpuid {}", cpu_id);
+
+    axhal::cpu::init_primary(cpu_id);
 
     let start = align_up_4k(virt_to_phys(_ekernel as usize));
     let end = align_down_4k(axconfig::PHYS_MEMORY_END);
@@ -29,6 +32,7 @@ pub extern "Rust" fn runtime_main(cpu_id: usize, _dtb_pa: usize) {
         axhal::cpu::set_current_task_ptr(ptr);
     }
 
+    // Init runq just for using mutex.
     run_queue::init();
 
     {
@@ -37,6 +41,18 @@ pub extern "Rust" fn runtime_main(cpu_id: usize, _dtb_pa: usize) {
 
         let main_fs = axmount::init_filesystems(disk, true);
         let root_dir = axmount::init_rootfs(main_fs);
+        let mut fs = FsStruct::new();
+        fs.init(root_dir);
+        axfile::api::create_dir("/testcases", &fs).unwrap();
+
+        let fname = "/testcases/new-file.txt";
+        info!("test create file {:?}:", fname);
+        //assert_err!(axfile::api::metadata(fname), NotFound);
+        let contents = "create a new file!\n";
+        axfile::api::write(fname, contents, &fs).unwrap();
+
+        let ret = axfile::api::read_to_string(fname, &fs).unwrap();
+        info!("read test file: \"{}\"", ret);
     }
 
     info!("[rt_axmount]: ok!");
